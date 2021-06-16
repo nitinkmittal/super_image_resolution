c12334bf079432290fa7d6c5cf9e50f66f970920
nitinkmittal <nitinmittal243@gmail.com>
2021-06-16 00:14:36 -0400

    Updated class name

--- src/super_image_resolution/transforms.py
+++ src/super_image_resolution/transforms.py
@@ -1,11 +1,15 @@
+"""Contains Pytorch styled image transformations."""
 from typing import Sequence, Union
 
 import numpy as np
 
 from torchvision.transforms import InterpolationMode, Resize
+from torch import Tensor
 
 
 class CustomResize(object):
+    """Added randomness to torchvision.transforms.Resize."""
+
     def __init__(self, size: Union[Sequence[int], int], interpolation=None):
         self.size = size
         self.interpolation = interpolation
@@ -23,3 +27,17 @@ class CustomResize(object):
 
     def __repr(self):
         return self.__class__.__name__ + f"Custom Resize"
+
+
+class InvertNormalize(object):
+    """Perform invert normalization."""
+
+    def __init__(self, num_channels, mean, std):
+        self.mean = mean.view(1, num_channels, 1, 1)
+        self.std = std.view(1, num_channels, 1, 1)
+
+    def __call__(self, img: Tensor) -> Tensor:
+        return (img * self.std) + self.mean
+
+    def __repr(self):
+        return self.__class__.__name__ + f"(mean: {self.mean}, std: {self.std})"
